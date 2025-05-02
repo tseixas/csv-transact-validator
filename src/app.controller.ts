d3430ba6e0dec2aa610d2a8eae57b90ac968eca6
Thiago Seixas <thiago.seixas10@gmail.com>
2025-05-02 19:01:16 -0400

    feat: adding swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ import {
 import { AppService } from './app.service';
 import { Upload } from './app.interface';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes, ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
@@ -14,6 +15,19 @@ export class AppController {
 
   @Post()
   @UseInterceptors(FileInterceptor('file'))
+  @ApiOperation({ summary: 'Upload do arquivo CSV' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
   upload(@UploadedFile() file: Express.Multer.File): Promise<Upload> {
     return this.appService.processFile(file);
   }
